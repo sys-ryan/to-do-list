2b0147b3e2acb0535d14e291f932d4aa32456cbb
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2021-02-24 23:30:10 +0900

    feat: set database
    - install mongoose
    - create itemSchema
    - create Item model
    - delete date feature

--- app.js
+++ app.js
@@ -1,34 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
 app.set('view engine', 'ejs');
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
 
 app.get("/", function(req, res){
 
-  const day = date.getDay();
-  console.log(day);
-  console.log(date.getDate());
-  res.render('list', {title: day, items: items});
+
+  Item.find({}, function(err, foundItems){
+    if(err){
+      console.log(err);
+    }else {
+
+      if(foundItems.length === 0){
+        Item.insertMany(defaultItems, function(err){
+          if(err){
+            console.log(err);
+          }
+        });
+      }else {
+        res.render('list', {title: "Today", items: foundItems});
+      }
+    }
+  })
+
+
 });
 
 app.post("/", function(req, res){
   const item =req.body.newItem;
-  if(req.body.list === "Work"){
-    workItems.push(item);
-    res.redirect("/Work");
-  }else {
-    items.push(item);
-    res.redirect("/");
-  }
-  console.log(req.body);
+  // if(req.body.list === "Work"){
+  //   workItems.push(item);
+  //   res.redirect("/Work");
+  // }else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
+  // console.log(req.body);
 });
 
 app.get("/Work", function(req, res){
